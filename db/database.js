be24762ec8e4d643168350dbbb093654a4767e6b
Andrew Jacob <amj1108@icloud.com>
2020-02-27 03:14:43 -0700

    update db

--- db/database.js
+++ db/database.js
@@ -1,5 +1,5 @@
 const PGClient = require("pg").Client
-const url = "postgres://localhost:5432/devdb"
+const url = process.env.DATABASE_URL || "postgres://localhost:5432/devdb"
 
 const defaultUsers = require("./default_users.json")
 
